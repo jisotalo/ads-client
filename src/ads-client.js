f1a5a07bd0c847b8a92a512c8b99b78eb7838b88
Jussi Isotalo <j.isotalo91@gmail.com>
2021-06-27 09:57:53 +0300

    Version 1.10.7
    ## [1.10.7] - 27.06.2021
    ### Changed
    - Bug fix: `connection.connected` flag was staying `true` if connection failed during initial connecting. ([See pull request #67](https://github.com/jisotalo/ads-client/pull/67))
    - Bug fix: Set `connection.connected` only after successful ADS port register

--- src/ads-client.js
+++ src/ads-client.js
@@ -315,6 +315,9 @@ class Client extends EventEmitter {
         socket.removeAllListeners()
         socket = null
 
+        //Reset connection flag
+        this.connection.connected = false
+
         reject(new ClientException(this, 'connect()', `Connection to ${this.settings.routerAddress}:${this.settings.routerTcpPort} failed (socket error ${err.errno})`, err))
       })
 
@@ -328,6 +331,9 @@ class Client extends EventEmitter {
         socket.removeAllListeners()
         socket = null
 
+        //Reset connection flag
+        this.connection.connected = false
+
         reject(new ClientException(this, 'connect()', `Connection to ${this.settings.routerAddress}:${this.settings.routerTcpPort} failed - socket closed by remote (hadError = ${hadError})`))
       })
 
@@ -338,7 +344,10 @@ class Client extends EventEmitter {
  
         //Remove all events from socket
         socket.removeAllListeners()
- 
+        socket = null
+
+        //Reset connection flag
+        this.connection.connected = false
  
         if (this.settings.localAdsPort != null)
           reject(new ClientException(this, 'connect()', `Connection to ${this.settings.routerAddress}:${this.settings.routerTcpPort} failed - socket ended by remote (is the given local ADS port ${this.settings.localAdsPort} already in use?)`))
@@ -356,8 +365,10 @@ class Client extends EventEmitter {
         
         //Remove all events from socket
         socket.removeAllListeners()
-
         socket = null
+
+        //Reset connection flag
+        this.connection.connected = false
 
         reject(new ClientException(this, 'connect()', `Connection to ${this.settings.routerAddress}:${this.settings.routerTcpPort} failed (timeout) - No response from router in ${this.settings.timeoutDelay} ms`))
       })
@@ -373,7 +384,6 @@ class Client extends EventEmitter {
         socket.setTimeout(0);
 
         this._internals.socket = socket
-        this.connection.connected = true
 
         //Try to register an ADS port
         try {
@@ -390,6 +400,8 @@ class Client extends EventEmitter {
           socket.destroy()
           //Remove all events from socket
           socket.removeAllListeners()
+          this.connection.connected = false
+
           return reject(new ClientException(this, 'connect()', `Registering ADS port from router failed`, err))
         }
 
